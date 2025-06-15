a953bf4bd606bb4ff7935a3b6be56a2ac3f11c8b
Pratyushagunna <pratyushagunna@gmail.com>
2025-06-15 17:25:57 +0530

    Initial commit

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Header.css'
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className="header-contents">
+        <h2>Order your favourite food here</h2>
+        <p>Choose from a diverse menu featruing a delecateble array of dishes crafted with the finest ingredients and culinary experties.Our mission is to bring you unforgettable flavors that blend tradition with innovation, to satisfy your craving and elevate your dining experience, one delicious meal at a time.</p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
